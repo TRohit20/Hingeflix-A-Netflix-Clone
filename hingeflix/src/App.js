cd3bfff90b37c0c691e7097e0dc363dde1fba526
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-23 12:18:35 +0530

    React Router and implementation

--- hingeflix/src/App.js
+++ hingeflix/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <HomeScreen/> 
+    <div className="app"> 
+      <Router>
+        <Routes>
+          {/* <Route exact path="/test" element = {<h1>THis is s test man</h1>}/> */}
+          <Route exact path="/" element={<HomeScreen/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
